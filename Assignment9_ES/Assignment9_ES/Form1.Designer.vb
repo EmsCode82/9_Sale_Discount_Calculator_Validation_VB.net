9045b5d8231e0263659291acada7cb085f744b81
EmsCode82 <emsgithub@gmail.com>
2024-01-09 15:16:38 -0500

    first commit

---
+++ Assignment9_ES/Assignment9_ES/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSalesPrice
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.lblSalesPrice = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtPercentOff = New System.Windows.Forms.TextBox()
+        Me.txtOriginalPrice = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(180, 201)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(1)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(59, 27)
+        Me.btnExit.TabIndex = 15
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(103, 201)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(1)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(59, 27)
+        Me.btnClear.TabIndex = 14
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(26, 201)
+        Me.btnSubmit.Margin = New System.Windows.Forms.Padding(1)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(59, 27)
+        Me.btnSubmit.TabIndex = 13
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'lblSalesPrice
+        '
+        Me.lblSalesPrice.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSalesPrice.Location = New System.Drawing.Point(54, 122)
+        Me.lblSalesPrice.Margin = New System.Windows.Forms.Padding(1, 0, 1, 0)
+        Me.lblSalesPrice.Name = "lblSalesPrice"
+        Me.lblSalesPrice.Size = New System.Drawing.Size(159, 40)
+        Me.lblSalesPrice.TabIndex = 12
+        Me.lblSalesPrice.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(52, 74)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(1, 0, 1, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Percent Off:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(52, 32)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(1, 0, 1, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Original Price:"
+        '
+        'txtPercentOff
+        '
+        Me.txtPercentOff.Location = New System.Drawing.Point(145, 71)
+        Me.txtPercentOff.Margin = New System.Windows.Forms.Padding(1)
+        Me.txtPercentOff.Name = "txtPercentOff"
+        Me.txtPercentOff.Size = New System.Drawing.Size(71, 20)
+        Me.txtPercentOff.TabIndex = 9
+        '
+        'txtOriginalPrice
+        '
+        Me.txtOriginalPrice.Location = New System.Drawing.Point(145, 30)
+        Me.txtOriginalPrice.Margin = New System.Windows.Forms.Padding(1)
+        Me.txtOriginalPrice.Name = "txtOriginalPrice"
+        Me.txtOriginalPrice.Size = New System.Drawing.Size(71, 20)
+        Me.txtOriginalPrice.TabIndex = 8
+        '
+        'frmSalesPrice
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(265, 259)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.lblSalesPrice)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPercentOff)
+        Me.Controls.Add(Me.txtOriginalPrice)
+        Me.Name = "frmSalesPrice"
+        Me.Text = "Sales Price"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents lblSalesPrice As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtPercentOff As TextBox
+    Friend WithEvents txtOriginalPrice As TextBox
+End Class
---
+++ Assignment9_ES/Assignment9_ES/Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class frmSalesPrice
+
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        ' Declare Variables
+        Dim dblOriginalPrice As Double
+        Dim dblPercentOff As Double
+        Dim dblSalesPrice As Double
+
+
+        ' Get and Validate Inputs
+        ' Original Price & Percent Off must Exist, The Data Type must be numeric, and The Range must be greater than 0
+
+
+        If txtOriginalPrice.Text = String.Empty Then
+            MessageBox.Show("Original Price Must be Entered")
+            txtOriginalPrice.Focus()
+            Exit Sub
+        End If
+
+        If txtPercentOff.Text = "" Then
+            MessageBox.Show("Percent Off Must be Entered")
+            txtPercentOff.Focus()
+            Exit Sub
+        End If
+
+        If IsNumeric(txtOriginalPrice.Text) Then
+            dblOriginalPrice = txtOriginalPrice.Text
+        Else
+            MessageBox.Show("Original Price Must be Numeric")
+            txtOriginalPrice.Focus()
+            Exit Sub
+        End If
+
+        If (dblOriginalPrice > 0) Then
+            dblOriginalPrice = txtOriginalPrice.Text
+        Else
+            MessageBox.Show("Original Price Must be Greater than 0")
+            Exit Sub
+        End If
+
+        If IsNumeric(txtPercentOff.Text) Then
+            dblPercentOff = txtPercentOff.Text
+        Else
+            MessageBox.Show("Percent Off Must be Numeric")
+            Exit Sub
+        End If
+
+        If (dblPercentOff > 0) Then
+            dblPercentOff = txtPercentOff.Text
+        Else
+            MessageBox.Show("Percent Off Must be Greater than 0")
+            Exit Sub
+        End If
+
+        ' Do Calculations
+        dblSalesPrice = dblOriginalPrice - ((dblOriginalPrice / 100) * dblPercentOff)
+
+
+        ' Display Results
+        lblSalesPrice.Text = FormatCurrency(dblSalesPrice)
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtOriginalPrice.Clear()
+        txtPercentOff.Clear()
+        txtOriginalPrice.Focus()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+
+    End Sub
+End Class
